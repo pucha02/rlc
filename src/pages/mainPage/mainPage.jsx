92b39f6a8219e978368aec143794174a71ba960f
Your Name <mp848695@gmail.com>
2024-08-26 17:08:44 +0300

    version - 26.08.2024

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,19 +1,33 @@
-import Languages from "../chooseLanguage/language";
-import Teachers from "../chooseTeacher/teachers"
-import Date from "../chooseDate/date";
-import Courses from "../chooseCourse/courses";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
 import { lazy } from "react";
-
+import { useState } from "react";
+var i = 0
 const MainPage = () => {
- return (
+  const Languages = lazy(() => import("../chooseLanguage/language"))
+  const Teachers = lazy(() => import("../chooseTeacher/teachers"))
+  const Date = lazy(() => import("../chooseDate/date"))
+  const Courses = lazy(() => import("../chooseCourse/courses"))
+
+  const [component, setComponent] = useState(null);
+
+  const arr = [<Languages />, <Teachers/>, <Date/>, <Courses/>]
+
+
+  const switchTab = () => {
+    setComponent(arr[i])
+    i++
+  }
+
+  return (
     <div>
       <p>MainPage</p>
-      <Languages/>
-      <Courses/>
-      <Teachers/>
-      <Date/>
+      <Suspense fallback={<div>Loading...</div>}>
+        {Languages}
+        {component}
+      </Suspense>
+      <button onClick={switchTab}>Next</button>
     </div>
   );
 };
